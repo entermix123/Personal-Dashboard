76f176fe89569fb671876db1cbd35d8e12539e47
entermix123 <balabanovjordan@gmail.com>
2024-08-05 22:11:43 +0300

    added Contact us component

--- client/src/components/contact-us/ContactUs.jsx
+++ client/src/components/contact-us/ContactUs.jsx
@@ -17,7 +17,6 @@ export default function ContactUs() {
             style={{ filter: "grayscale(1) contrast(1.2) opacity(0.4)" }}
             />
         </div>
-        <HeaderNavbar />
         <div className="container px-5 py-24 mx-auto flex">
             <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-1000 shadow-md">
             <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
