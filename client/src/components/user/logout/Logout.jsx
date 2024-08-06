1f18ec39c90b75d43594207c9b3379d4ca7d4d12
entermix123 <balabanovjordan@gmail.com>
2024-08-06 21:02:57 +0300

    fixed logout

--- client/src/components/user/logout/Logout.jsx
+++ client/src/components/user/logout/Logout.jsx
@@ -1,13 +1,10 @@
 import { Navigate } from 'react-router-dom'
 import { useLogout } from '../../../hooks/useAuth'
-import { useEffect } from 'react';
 
 export default function Logout() {
     const logout = useLogout();
 
-    useEffect(() => {
-        logout(); // Call the logout function when the component mounts
-    }, [logout]);
+    logout(); // Call the logout function when the component mounts
 
     return <Navigate to="/" />              // redirect to home page
 }
