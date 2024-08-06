d0994086c07d17d8eb5f3757c5071b5b3991cd35
entermix123 <balabanovjordan@gmail.com>
2024-08-06 22:42:02 +0300

    removed button from about us page, edited readem and work notes

--- client/src/components/about-us/AboutUs.jsx
+++ client/src/components/about-us/AboutUs.jsx
@@ -117,9 +117,6 @@ export default function AboutUs() {
                     </a>
                 </div>
                 </div>
-                <button className="flex mx-auto mt-20 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                Button
-                </button>
             </div>
         </section>
     );
